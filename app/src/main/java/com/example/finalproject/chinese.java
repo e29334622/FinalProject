d5d22ae326a808db78a4ced914f9b7d140bcd54b
e29334622 <e29334622@yahoo.com.tw>
2020-12-26 22:17:11 +0800

    second

--- app/src/main/java/com/example/finalproject/chinese.java
+++ app/src/main/java/com/example/finalproject/chinese.java
@@ -7,12 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 public class chinese extends AppCompatActivity {
@@ -34,12 +32,10 @@ public class chinese extends AppCompatActivity {
         public int getCount() {
             return data.length;
         }
-
         @Override
         public Object getItem(int position) {
             return data[position];
         }
-
         @Override
         public long getItemId(int position) {
             return 0;
@@ -75,8 +71,9 @@ public class chinese extends AppCompatActivity {
 
         String[] mealNameArray = new String[]{"大麥克", "雙層牛肉吉事堡", "嫩煎雞腿堡", "麥香雞", "麥克雞塊(6塊)", "麥克雞塊(10塊)", "勁辣雞腿堡", "麥脆雞翅",
                 "麥脆雞腿", "黃金起司豬排堡", "麥香魚", "煙燻雞肉長堡", "醬燒豬肉長寶堡", "蕈菇安格斯黑牛堡", "BLT安格斯黑牛堡", "BLT辣脆雞腿堡", "BLT嫩煎雞腿堡", "凱撒辣脆雞沙拉", "義式烤雞沙拉"};
-        int[] mealPhotoArray = new int[]{R.drawable.meal1, R.drawable.meal1, R.drawable.meal1, R.drawable.meal1, R.drawable.meal1, R.drawable.meal1, R.drawable.meal1, R.drawable.meal1,
-                R.drawable.meal1, R.drawable.meal1, R.drawable.meal1, R.drawable.meal1, R.drawable.meal1, R.drawable.meal1, R.drawable.meal1, R.drawable.meal1, R.drawable.meal1, R.drawable.meal1, R.drawable.meal1};
+        int[] mealPhotoArray = new int[]{R.drawable.a_01_big_bac, R.drawable.a_02_cheeseburger, R.drawable.a_03_gbc, R.drawable.a_04_mcchicken, R.drawable.a_05_ngt6, R.drawable.a_06_ngt10,
+                R.drawable.a_07_scf, R.drawable.a_08_mfc, R.drawable.a_09_mfc, R.drawable.a_10_pork_burger, R.drawable.a_11_fof, R.drawable.a_12_smoked_chicken, R.drawable.a_13_ginger_pork,
+                R.drawable.a_14_mushroom, R.drawable.a_15_beef, R.drawable.a_16_fried_chicken, R.drawable.a_17_grilled_chicken, R.drawable.a_18_spicy_fried_chicken, R.drawable.a_19_grilled_chicken};
 
         Data[] mealData = new Data[mealNameArray.length];
         for(int i=0 ; i<mealData.length ; i++){
@@ -85,7 +82,7 @@ public class chinese extends AppCompatActivity {
             mealData[i].photo = mealPhotoArray[i];
         }
 
-        MyAdapter mealAdapter = new MyAdapter(mealData,R.layout.meal);
+        MyAdapter mealAdapter = new MyAdapter(mealData,R.layout.custom);
 
         ListView listView = findViewById(R.id.listview);
         listView.setAdapter(mealAdapter);
